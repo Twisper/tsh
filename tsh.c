62f6ed59b1a6e2e2645ea5498831fe6e7b560799
Twisper <83642379+Twisper@users.noreply.github.com>
2025-12-27 17:15:56 +0300

    Feat: Added displaying current working directory in shell

--- tsh.c
+++ tsh.c
@@ -10,18 +10,57 @@
 
 #define	MAXLINE 8192
 #define MAXARGS 128
-#define NAME_MAXLEN 128
+
+static void eval(char *cmdline);
+static int builtin_command(char **argv);
+static int parseline(char *buf, char **argv);
+static void cd(char **argv);
+static void export(char **argv);
+static char *extract_pwd(char *pwd);
 
 int main() {
 
     char cmdline[MAXLINE];
     char *username = getenv("USER");
     char *hostname = getenv("HOSTNAME");
+    char *pwd;
+    char homedir = "~";
 
     while (1) {
-        printf("%s@%s:~$", username, hostname);
+        pwd = extract_pwd(getenv("PWD"));
+        if (!strcmp(pwd, username))
+            pwd = &homedir;
+        printf("%s@%s: %s %% ", username, hostname, pwd);
         fgets(cmdline, MAXLINE, stdin);
         if (feof(stdin))
             continue;
+
+        eval(cmdline);
     }
+}
+
+static char *extract_pwd(char *pwd) {
+    char *ret_ptr = pwd;
+    int i = strlen(pwd);
+
+    while (ret_ptr[i] != '/') {
+        i--;
+    }
+
+    return ret_ptr + i + 1;
+}
+
+static void eval(char *cmdline) {
+
+}
+
+static int builtin_command(char **argv) {
+    if (!strcmp(argv[0], "exit"))
+        exit(0);
+    if (!strcmp(argv[0], "&"))
+        return 1;
+    if (!strcmp(argv[0], "cd"))
+        cd(argv);
+    if (!strcmp(argv[0], "export"))
+        export(argv);
 }
